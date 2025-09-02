b5b847bc488dc44d788a1bbb34b504e3b7532ad0
Aryan Agarwal <aryankush025@gmail.com>
2025-09-02 19:40:59 +0530

    Adds EC2 instance configuration with AWS provider setup
    Initializes AWS provider with empty credentials placeholder and defines a basic EC2 instance resource with default AMI and t3.micro type. This establishes the foundational infrastructure for deploying an EC2 instance in the Terraform configuration.

--- first_ec2.tf
+++ first_ec2.tf
@@ -8,5 +8,16 @@ terraform {
 }
 
 provider "aws" {
-  # Configuration options
+  region = ""
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_instance" "my_ec2" {
+  ami = "ami-00ca32bbc84273381"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "my-first-ec2"
+  }
 }
