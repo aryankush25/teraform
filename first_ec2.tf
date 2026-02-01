da6b53370aa38c8699d308e482776a006e16679f
Aryan Agarwal <aryankush025@gmail.com>
2026-02-01 16:26:12 +0530

    Enable EC2 instance creation
    This commit uncommented the resource block for `aws_instance.my_ec2` in
    `first_ec2.tf`, enabling the creation of the EC2 instance.

--- first_ec2.tf
+++ first_ec2.tf
@@ -6,11 +6,11 @@ resource "aws_iam_user" "demouser" {
   name = "demouser-aryankush25"
 }
 
-# resource "aws_instance" "my_ec2" {
-#   ami = "ami-04233b5aecce09244"
-#   instance_type = "t3.micro"
+resource "aws_instance" "my_ec2" {
+  ami = "ami-04233b5aecce09244"
+  instance_type = "t3.micro"
 
-#   tags = {
-#     Name = "my-first-ec2"
-#   }
-# }
+  tags = {
+    Name = "my-first-ec2"
+  }
+}
