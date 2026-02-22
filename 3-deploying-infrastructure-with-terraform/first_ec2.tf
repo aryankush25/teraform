265b5b81db2c6900f4ded70604d057f5150e96f8
Aryan Agarwal <aryankush025@gmail.com>
2026-02-22 16:14:29 +0530

    Update EC2 AMI
    Updates the Amazon Machine Image (AMI) used for the EC2 instance.

--- 3-deploying-infrastructure-with-terraform/first_ec2.tf
+++ 3-deploying-infrastructure-with-terraform/first_ec2.tf
@@ -7,7 +7,7 @@ resource "aws_iam_user" "demouser" {
 }
 
 resource "aws_instance" "my_ec2" {
-  ami = "ami-04233b5aecce09244"
+  ami           = "ami-04233b5aecce09244"
   instance_type = "t3.micro"
 
   tags = {
